5e6dbf4de8f9e234c6ee5c88f0a8a53acc277463
alpaolo <paolo.alberti@clipart.it>
2021-02-07 17:44:38 +0100

    image curl xfer ok

--- git_add.bat
+++ git_add.bat
@@ -7,6 +7,7 @@ git rm --cached aimagine\yolov3-wider_16000.weights
 git rm --cached .\aimagine\yolov3-wider.cfg
 git rm --cached aimagine\coco.names.wider
 git rm --cached aimagine\people.jpg
+git rm --cached img.jpg
 
 git status
 
