1380439f2e082bc06488ba9799a66a5256c3a2ba
alpaolo <paolo.alberti@clipart.it>
2021-03-01 21:34:40 +0100

    last revision

--- git_add.bat
+++ git_add.bat
@@ -8,6 +8,8 @@ git rm --cached .\aimagine\yolov3-wider.cfg
 git rm --cached aimagine\coco.names.wider
 git rm --cached aimagine\people.jpg
 git rm --cached img.jpg
+git rm --cached aimagine\static\media\Cattura.PNG
+git rm --cached aimagine\static\media\rekog_Cattura.PNG
 
 git status
 
