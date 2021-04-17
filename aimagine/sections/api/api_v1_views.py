28ea1dbc0e748b77c1d94222602066cd41021a45
alpaolo <paolo.alberti@clipart.it>
2021-04-17 22:13:34 +0200

    running code

--- aimagine/sections/api/api_v1_views.py
+++ aimagine/sections/api/api_v1_views.py
@@ -3,7 +3,9 @@ import json
 import base64
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpRequest
 from django.http import JsonResponse
+
 from django.core.files.storage import FileSystemStorage
 from django.conf import settings as conf_settings
 from django.views.decorators.csrf import csrf_exempt
@@ -79,25 +81,50 @@ Gestire il request ed inviare alla funzione aws il bytearray ricevuto
 @csrf_exempt
 @xframe_options_exempt
 def aws_faceblur(request):
-    print(request.FILES['file'])
+    print(request)
     #\  img.show()
     #\  detector = YoloDetector(img_to_detect=img)
     #\  result = detector.run_singleframe_detector()
-    result = aws_detector(request.FILES['file'].read()) # Processa l'immagine
-    
-    args['items'] = result['items'] # Lista di elementi riconosciuti
-    args['n_items'] = result['n_items'] # Numero di elementi riconosciuti
-    #\  args['rekog_imgsrc'] =  'media/'+result['rekog_filename']
-
-    result['message'] = "Ho trovato i visi"
+    imgdata = request.FILES['file'].read()
+    client=boto3.client('rekognition')
+    result = client.detect_faces(Image={'Bytes': imgdata})['FaceDetails']
+    #result['message'] = "Ho trovato i visi"
     #\  print (result)
-    return JsonResponse(result)
+    return HttpResponse(result)
+
+
+@csrf_exempt
+@xframe_options_exempt
+def lambda_faceblur(request):
+    import requests
+    #print(request.FILES['file'])
+    #\  img.show()
+    #\  detector = YoloDetector(img_to_detect=img)
+    #\  result = detector.run_singleframe_detector()
+    #result = aws_detector(request.FILES['file'].read()) # Processa l'immagine
+    url = 'https://qxv017uuj5.execute-api.eu-west-1.amazonaws.com/default/detectFace'
+    img_data = request.FILES['file'].read()
+    #print(img_data)
+    b64string_img_data  =base64.b85encode(img_data).decode('utf-8')
+    payload = {
+            'imageName': 'image_name',
+            'imageBytes': b64string_img_data,
+    }
+    response = requests.post(url, data=img_data)
+    response_data = response.json()
+    print ("result: ",response_data)
+    
+    #args['items'] = response_data['items'] # Lista di elementi riconosciuti
+    #args['n_items'] = response_data['n_items'] # Numero di elementi riconosciuti
+    #\  args['rekog_imgsrc'] =  'media/'+result['rekog_filename']
+    #\  print (result)
+    return JsonResponse(response_data, safe=False)
 
 
     
 '''
 Modificare il codice di aws_detector in modo che prelevi il bytearray dal Request e lo invii ad aws 
-'''
+INUTILIZZABILI '''  
 # Call AWS Rekognition
 def aws_detector_with_string_data(string_imgdata): # va tutto spostato in models
         # Rileva il percorso assoluto per leggere l'immagine da processare
